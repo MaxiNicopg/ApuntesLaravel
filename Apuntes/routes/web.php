8153ac7d144b407a8dc1da2c17625821ed0939cf
Maximiliano Fuenzalida <62958643+MaxiNicopg@users.noreply.github.com>
2021-04-27 00:59:34 -0400

    Todo lo que tiene que ver con rutas en web.php

--- Apuntes/routes/web.php
+++ Apuntes/routes/web.php
@@ -14,5 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Bienvenido a la pagina principal";
+});
+
+Route::get('cursos', function(){
+    return "Bienvenidos a la pagina cursos";
+});
+
+//Ruta con el formulario para poder crear un curso.
+/* Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+}); */
+
+//DAR Y RECIBIR VARIABLES EN LA URL
+/* Route::get('cursos/{curso}',function($curso){
+    return "bienvenido al curso: $curso";
+}); */
+
+//En esta ruta, la categoria puede ser opcional, así que al final del nombre de la 
+//variable se le pone un signo de interrogación y en la función se inicializa y se le da el valor null.
+Route::get('cursos/{curso}/{categoria?}',function($curso, $categoria = null){
+
+    if($categoria){
+        return "bienvenido al curso: $curso, de la categoría $categoria";
+    }else{
+        return "bienvenido al curso: $curso";
+    }
 });
